f792e77f47fc3fbd71a73798612aebf017389b1e
Emre Demirhan <info@emredemirhan.com>
2025-03-05 11:32:48 +0300

    feat(frontend): set up React application with routing

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App d-flex flex-column min-vh-100">
+      </div>
+    </Router>
+  );
+}
+
+export default App; 
